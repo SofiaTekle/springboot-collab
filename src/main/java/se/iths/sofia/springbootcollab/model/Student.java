1fe901e5ac1b917fab083fcc1e22f9e233a75665
SaraSoltani98 <SaraSoltani.SaraSoltani@iths.se>
2026-01-27 20:08:02 +0100

    Student

---
+++ src/main/java/se/iths/sofia/springbootcollab/model/Student.java
@@ -0,0 +1,26 @@
+package se.iths.sofia.springbootcollab.model;
+
+public class Student {
+
+    private String firstName;
+    private String lastName;
+    private String program;
+
+    public Student(String firstName, String lastName, String program) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.program = program;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+}
