0ac476ca8d8858eb6138344cfb01f482513f5918
SofiaTekle <sofiatekle@outlook.com>
2026-01-27 09:57:17 +0100

    initial commit

---
+++ src/test/java/se/iths/sofia/springbootcollab/SpringbootCollabApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.sofia.springbootcollab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCollabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
