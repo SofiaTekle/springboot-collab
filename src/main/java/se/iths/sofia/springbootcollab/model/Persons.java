fdb783d60997ea7ed977b132c205c31b3a541df6
SaraSoltani98 <SaraSoltani.SaraSoltani@iths.se>
2026-01-27 20:43:15 +0100

    Persons

---
+++ src/main/java/se/iths/sofia/springbootcollab/model/Persons.java
@@ -0,0 +1,34 @@
+package se.iths.sofia.springbootcollab.model;
+
+public class Persons {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int age;
+
+    public Persons(String firstName, String lastName, int age, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+
