67ec17d7a988b9efa469694c1e8129f74860867f
SofiaTekle <sofiatekle@outlook.com>
2026-01-27 10:41:01 +0100

    Add user flow with controller,model and view

---
+++ src/main/java/se/iths/sofia/springbootcollab/controller/UserController.java
@@ -0,0 +1,27 @@
+package se.iths.sofia.springbootcollab.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import se.iths.sofia.springbootcollab.model.User;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+
+
+    @GetMapping("/users")
+    public String getUsers(Model model) {
+        List<User> users = List.of(
+                new User(1L, "sofia", "sofia@example.com"),
+                new User(2L, "alex", "alex@example.com"),
+                new User(3L, "emma", "emma@example.com")
+        );
+
+        model.addAttribute("users", users);
+        return "users";
+    }
+
+
+}
