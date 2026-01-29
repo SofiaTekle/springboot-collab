3594b29d293294bbba07403d256c4a73df0f88e0
SaraSoltani98 <SaraSoltani.SaraSoltani@iths.se>
2026-01-29 10:46:39 +0100

    s

--- src/main/java/se/iths/sofia/springbootcollab/controller/HomeController.java
+++ src/main/java/se/iths/sofia/springbootcollab/controller/HomeController.java
@@ -4,9 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import se.iths.sofia.springbootcollab.model.Persons;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/")
@@ -15,21 +12,8 @@ public class HomeController {
 
     @GetMapping
     public String home(Model model) {
-        model.addAttribute("greeting", "Välkommen");
+        model.addAttribute("greeting", "Välkommen Sara");
         return "home";
-    }
-
-    @GetMapping("/persons")
-    public String getPersons(Model model) {
-
-        List<Persons> persons = List.of(
-                new Persons("Anna", "Eriksson", 25, "anna.eriksson@gmail.com"),
-                new Persons("Erik", "Eriksson", 30, "erik.eriksson@yahoo.com"),
-                new Persons("Samuel", "Svensson", 28, "sara.svensson@gmail.com")
-        );
-
-        model.addAttribute("persons", persons);
-        return "persons";
     }
 
 }
