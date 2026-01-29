af48dec6514bd32eee981717f28eb9121e0a056c
sedina-imamovic <Sedina.Imamovic@iths.se>
2026-01-29 10:36:19 +0100

    Ändra namnet till Sedina

--- src/main/java/se/iths/sofia/springbootcollab/controller/HomeController.java
+++ src/main/java/se/iths/sofia/springbootcollab/controller/HomeController.java
@@ -12,7 +12,7 @@ public class HomeController {
 
     @GetMapping
     public String home(Model model) {
-        model.addAttribute("greeting", "Välkommen Sedina!");
+        model.addAttribute("greeting", "Välkommen Sedina");
         return "home";
     }
 
