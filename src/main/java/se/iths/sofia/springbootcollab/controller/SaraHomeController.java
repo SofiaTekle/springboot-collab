52ff975ec5655b55f97a2b84408d7c926f6ee334
SaraSoltani98 <SaraSoltani.SaraSoltani@iths.se>
2026-01-29 10:35:58 +0100

    Sara

---
+++ src/main/java/se/iths/sofia/springbootcollab/controller/SaraHomeController.java
@@ -0,0 +1,22 @@
+package se.iths.sofia.springbootcollab.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class SaraHomeController {
+    @GetMapping
+    public String home(Model model) {
+        model.addAttribute("greeting", "Välkommen Sara");
+        return "home";
+    }
+}
+
+
+
+
+
+
