7a34c6d8f7bff904483f669a7d1df05611a93c6f
sedina-imamovic <Sedina.Imamovic@iths.se>
2026-01-29 14:06:01 +0100

    Välkomna

--- src/main/java/se/iths/sofia/springbootcollab/controller/HomeController.java
+++ src/main/java/se/iths/sofia/springbootcollab/controller/HomeController.java
@@ -4,9 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import se.iths.sofia.springbootcollab.model.Persons;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/")
@@ -15,7 +12,7 @@ public class HomeController {
 
     @GetMapping
     public String home(Model model) {
-        model.addAttribute("greeting", "Välkommen Sedina");
+        model.addAttribute("greeting", "Välkomna! ");
         return "home";
     }
 
